e972bad27cc447855366b67a4f4a7c8973d85069
songwell1024 <zysong0709@foxmail.com>
2019-05-24 21:36:26 +0800

    add a util functions and delete a bug in compare

--- Util/FSquaDRA-master.py
+++ Util/FSquaDRA-master.py
@@ -3,44 +3,46 @@ import os
 import datetime
 
 jarPath = r"E:\APKDataSet\FSquaDRA_master_jar\FSquaDRA-master.jar "
-apkDirs = r"E:\APKDataSet\xiaomi\1"
+apkDirs = r"E:\APKDataSet\xiaomi\7"
 myList = os.listdir(apkDirs)
 ignoring = open(r"E:\APKDataSet\compareResults\FSquaDRA\1\ignoring.txt", "a")
 result = open(r"E:\APKDataSet\compareResults\FSquaDRA\1\result.txt", "a")
 startTime = datetime.datetime.now()
-for apkName1 in myList[:-1]:
-	# ignoring = open(r"C:\Users\sp\Desktop\cmp\FSquaDRA\1\ignoring.txt", "a")
-	# result = open(r"C:\Users\sp\Desktop\cmp\FSquaDRA\1\result.txt", "a")
-	for apkName2 in myList[myList.index(apkName1)+1:]:
-		try:
-			apkPath1 = apkDirs + "\\" + apkName1 + " "
-			apkPath2 = apkDirs + "\\" + apkName2 + " "
-			resultPath = r"-o=E:\APKDataSet\compareResults\FSquaDRA\1\tmp.txt"
-			command = r"java -jar " + jarPath + apkPath1 + apkPath2 + resultPath
-			f = os.popen(command, "r")
-			d = f.read()
-			cmdList = d.split("\n")
-			if cmdList[cmdList.index("Skipped files:") + 1] == apkName1:
-				ignoring.write(apkName1 + "\n")
-				# print(d)
-				break
-			cmdList.reverse()
-			if cmdList[cmdList.index("Skipped files:") - 1] == apkName2:
-				# myList.remove(apkName2)
-				ignoring.write(apkName2 + "\n")
-				# print(d)
-				continue
-			tmp = open(r"E:\APKDataSet\compareResults\FSquaDRA\1\tmp.txt", encoding='UTF-8-sig')
-			if tmp != "\n":
-				APKSimStr = tmp.read()
-				APKSimStr = APKSimStr.strip('\n')
-				if APKSimStr.split(',')[-1] == 'same':
-					result.write(APKSimStr + '\n')
-			tmp.close()
-		except:
-			print(apkName1 + " " + apkName2)
-ignoring.close()
-result.close()
-
-endTime = datetime.datetime.now()
-print("time: " + str(endTime - startTime))
+for i in myList:
+	print(i)
+# for apkName1 in myList[:-1]:
+# 	# ignoring = open(r"C:\Users\sp\Desktop\cmp\FSquaDRA\1\ignoring.txt", "a")
+# 	# result = open(r"C:\Users\sp\Desktop\cmp\FSquaDRA\1\result.txt", "a")
+# 	for apkName2 in myList[myList.index(apkName1)+1:]:
+# 		try:
+# 			apkPath1 = apkDirs + "\\" + apkName1 + " "
+# 			apkPath2 = apkDirs + "\\" + apkName2 + " "
+# 			resultPath = r"-o=E:\APKDataSet\compareResults\FSquaDRA\1\tmp.txt"
+# 			command = r"java -jar " + jarPath + apkPath1 + apkPath2 + resultPath
+# 			f = os.popen(command, "r")
+# 			d = f.read()
+# 			cmdList = d.split("\n")
+# 			if cmdList[cmdList.index("Skipped files:") + 1] == apkName1:
+# 				ignoring.write(apkName1 + "\n")
+# 				# print(d)
+# 				break
+# 			cmdList.reverse()
+# 			if cmdList[cmdList.index("Skipped files:") - 1] == apkName2:
+# 				# myList.remove(apkName2)
+# 				ignoring.write(apkName2 + "\n")
+# 				# print(d)
+# 				continue
+# 			tmp = open(r"E:\APKDataSet\compareResults\FSquaDRA\1\tmp.txt", encoding='UTF-8-sig')
+# 			if tmp != "\n":
+# 				APKSimStr = tmp.read()
+# 				APKSimStr = APKSimStr.strip('\n')
+# 				if APKSimStr.split(',')[-1] == 'same':
+# 					result.write(APKSimStr + '\n')
+# 			tmp.close()
+# 		except:
+# 			print(apkName1 + " " + apkName2)
+# ignoring.close()
+# result.close()
+#
+# endTime = datetime.datetime.now()
+# print("time: " + str(endTime - startTime))
